ba16e66e12018bd7bfbd5f15b18c2b9c7bd50c7d
Caio <caio.vr12@gmail.com>
2026-04-02 07:31:46 -0300

    chore: sobe base para microserviço Spring Boot sem alterações

---
+++ src/test/java/caio/portfolio/design_pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package caio.portfolio.design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
