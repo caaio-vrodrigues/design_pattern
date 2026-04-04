6b85bd079b9f6a4316c91e9399c39b6971080067
Caio <caio.vr12@gmail.com>
2026-04-04 09:48:09 -0300

    feat: implementa validador para a entidade Product * ProductValidator:   - define ProductValidator como um componente Spring injetável.   - implementa a interface SalableComponentValidator com CreateProductCommand como tipo do comando.   - sobrescreve o método de validação de unicidade para novo Product.   - consulta o repositório para impedir duplicidade por name, brand e model.   - informa o tipo do componente como PRODUCT para validação polimórfica.   - lança exceção específica quando identifica tentativa de cadastro duplicado. * ProductRepository:   - cria interface Spring Data JPA para acessos ao banco de dados relacionados à entidade Product.   - expõe consulta para verificar existência de Product pela combinação dos campos: name, brand e model. * ProductAlreadyExistsException:   - representa a violação de unicidade ao tentar cadastrar um novo Product duplicado.

---
+++ src/main/java/caio/portfolio/design_pattern/domain/model/interfaces/product/ProductValidator.java
@@ -0,0 +1,32 @@
+package caio.portfolio.design_pattern.domain.model.interfaces.product;
+
+import org.springframework.stereotype.Component;
+
+import caio.portfolio.design_pattern.domain.exception.salable_component.product.ProductAlreadyExistsException;
+import caio.portfolio.design_pattern.domain.model.command.CreateProductCommand;
+import caio.portfolio.design_pattern.domain.model.enums.SalableComponentType;
+import caio.portfolio.design_pattern.domain.model.interfaces.SalableComponentValidator;
+import caio.portfolio.design_pattern.infrastructure.persistence.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProductValidator implements SalableComponentValidator<CreateProductCommand> {
+	
+	private final ProductRepository repo;
+	
+	@Override
+	public void validateSalableComponent(CreateProductCommand command) {
+		boolean productAlreadyExists = repo.existsByNameAndBrandAndModel(
+			command.getName(), 
+			command.getBrand(), 
+			command.getModel());
+		if(productAlreadyExists)
+			throw new ProductAlreadyExistsException("Falha de duplicação ao tentar salvar novo `Product`: [name: `"+command.getName()+"`; brand: `"+command.getBrand()+"`; model: `"+command.getModel()+"`].");
+	}
+
+	@Override
+	public SalableComponentType getType() {
+		return SalableComponentType.PRODUCT;
+	}
+}
