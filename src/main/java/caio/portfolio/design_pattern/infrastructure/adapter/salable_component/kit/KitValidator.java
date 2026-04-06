a80a4a2fc9c871117c34eea7846def658f724bac
Caio <caio.vr12@gmail.com>
2026-04-05 22:23:08 -0300

    feat: implementa validador e suporte de geração de código para Kit * KitValidator:   - define KitValidator como componente Spring injetável.   - implementa KitComponentValidator para o fluxo específico de Kit.   - gera o code do Kit a partir do maior id existente.   - valida a unicidade do code antes da criação.   - informa o tipo do componente como KIT para validação polimórfica.   - lança exceção específica quando identifica tentativa de cadastro duplicado. * KitRepository:   - cria interface Spring Data JPA para acessos ao banco de dados relacionados à entidade Kit.   - expõe consulta para verificar existência de Kit pelo code.   - expõe consulta para recuperar o maior id cadastrado e apoiar a geração sequencial do code. * KitAlreadyExistsException:   - representa a violação de unicidade ao tentar cadastrar um novo Kit duplicado.

---
+++ src/main/java/caio/portfolio/design_pattern/infrastructure/adapter/salable_component/kit/KitValidator.java
@@ -0,0 +1,39 @@
+package caio.portfolio.design_pattern.infrastructure.adapter.salable_component.kit;
+
+import org.springframework.stereotype.Component;
+
+import caio.portfolio.design_pattern.domain.exception.salable_component.kit.KitAlreadyExistsException;
+import caio.portfolio.design_pattern.domain.model.enums.SalableComponentType;
+import caio.portfolio.design_pattern.domain.model.interfaces.KitComponentValidator;
+import caio.portfolio.design_pattern.infrastructure.persistence.repository.KitRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class KitValidator implements KitComponentValidator {
+	
+	private final KitRepository repo;
+	
+	private String generateCode() {
+		Long maxId = repo.findMaxId();
+		Long codeNumber = maxId == null ? 0 : maxId;
+		return String.valueOf(codeNumber + 1);
+	}
+	
+	private void validateCode(String code) {
+		if(repo.existsByCode(code))
+			throw new KitAlreadyExistsException("Falha de duplicidade ao tentar criar `Kit`: [code: `"+code+"`].");
+	}
+
+	@Override
+	public String generateAndValidateCode() {
+		String code = generateCode();
+		validateCode(code);
+		return code;
+	}
+	
+	@Override
+	public SalableComponentType getType() {
+		return SalableComponentType.KIT;
+	}
+}
